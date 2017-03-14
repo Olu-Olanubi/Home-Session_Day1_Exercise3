e3a1a038d2df14fc63484b439b46012c9a124f9c
Olu-Olanubi <seyeolanubi@gmail.com>
2017-03-14 01:46:37 +0100

    day1_exercise3

---
+++ spec/classSpec.js
@@ -0,0 +1,76 @@
+var Company = require('../src/company.js');
+(function(){
+  'use strict';
+  describe("Company Class: Create a company, and assign its profile", function() {
+
+    it("The company should be a type of `object`, and an instance of the `Company` class", function() {
+      var company = new Company('Andela');
+      expect(typeof company).toEqual(typeof {});
+      expect(company instanceof Company).toBeTruthy();
+    });
+});
+
+    describe("Company Class: Create a company, and assign its profile", function() {
+
+    it("The company name and location should be a property of the company", function() {
+      var company  = new Company('Andela', 'Lagos');
+      expect(company.name).toBe('Andela');
+      expect(company.location).toBe('Lagos');
+  	});
+  });
+
+    describe("Company Class: Create a company, and assign its profile", function() {
+
+  	it("The company name and location and other parameters should be a property of the company", function() {
+      var company  = new Company('Cisco', 'San-Francisco', 'Networking');
+      expect(company.name).toBe('Cisco');
+      expect(company.location).toBe('San-Francisco');
+      expect(company.business).toBe('Networking');
+  	});
+  });
+    describe("Company Class: Create a company, and assign its profile", function() {
+
+  	it("The company name and location and other parameters should be a property of the company", function() {
+      var company  = new Company('Google', 'California', 'Web Services', 15000);
+      expect(company.name).toBe('Google');
+      expect(company.location).toBe('California');
+      expect(company.business).toBe('Web Services');
+      expect(company.staff).toEqual(15000);
+      expect(company.size).toBe('Multinational');
+  	});
+
+});
+    describe("Company Class: Create a company, and assign its profile", function() {
+    	it("The company name and location and other parameters should be a property of the company", function() {
+
+    		var company = new Company('Andela', 'Lagos', 'Software', 100);
+    		expect(company.size).toBe('SME');
+    	});
+
+      it("The company name and location and other parameters should be a property of the company", function() {
+
+        var company = new Company('Microsoft', 'Washington', 'Software', 500);
+        expect(company.size).toBe('Big Company');
+      });
+
+      it("The company name and location and other parameters should be a property of the company", function() {
+
+        var company = new Company('Microsoft', 'Washington', 'Software', 500);
+        expect(company.industry).toBe('Technology');
+      });
+      it("The company name and location and other parameters should be a property of the company", function() {
+      var company = new Company('CitiBank', 'New York', 'Banking', 5000);
+
+        expect(company.industry).toBe('Finance');
+      });
+    
+    });
+
+    describe("Company Class: Create a company, and assign its profile", function() {
+    	it("The company name and location and other parameters should be a property of the company", function() {
+
+    		var company = new Company('Andela', 'Lagos', 'Software', 120);
+    		expect(company.profile).toBe('We are Andela, headquartered in Lagos. We are into Software and our staff strength is 120. We are a Big Company');
+    	});
+    });
+})();
---
+++ src/company.js
@@ -0,0 +1,42 @@
+
+//creates a class company with certain parameters.
+//It is a simple class to demonstrate OOP implementation in Javascript.
+'use strict'
+function Company(name, location, business, employees){
+		this.name = name;
+		this.location = location;
+		this.business = business;
+		this.staff = employees;
+		
+		if (employees<=100){
+			this.size = 'SME';
+			}
+		if (employees > 100 &&  employees <= 1000){
+			this.size ='Big Company';
+			 }
+		if (employees > 1000){
+			this.size = 'Multinational';
+			}
+
+		 var finance = ['Banking', 'Insurance', 'Stockbroking', 'Commodities', 'Forex']; 
+		 var tech =  ['Software','Web Services', 'Internet', 'eCommerce', 'Electronics', 'Computers', 'Mobile'];
+			for( var i = 0; i < finance.length; i++){
+				if(business === finance[i]){
+					this.industry = 'Finance';
+					}
+				else{
+					for (var i = 0; i < tech.length; i++){
+					if (business === tech[i]){
+							this.industry = 'Technology';
+						}
+					}	
+					
+					}
+				}			
+	
+				
+this.profile = "We are " + this.name + ", headquartered in " + this.location + ". We are into " + this.business 
+		+ " and our staff strength is " + this.staff + ". We are a " + this.size;
+
+}
+module.exports = Company;
